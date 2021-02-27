fa2a1af6f98a7a2ae20341773d456dddd1f359fb
Ian Brandt <ian@ianbrandt.com>
2021-02-26 22:34:06 -0800

    Upgraded to Compose Desktop 0.3.1.

--- build.gradle.kts
+++ build.gradle.kts
@@ -5,7 +5,7 @@ import org.jetbrains.kotlin.gradle.tasks.KotlinCompile
 
 plugins {
 	kotlin("jvm") version "1.4.30"
-	id("org.jetbrains.compose") version "0.3.0"
+	id("org.jetbrains.compose") version "0.3.1"
 }
 
 group = "org.sdkotlin"
