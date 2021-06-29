bb7edc1f53e150453fcdc99345f909007d400ca1
Ian Brandt <ian.r.brandt@saic.com>
2021-06-29 07:34:10 -0700

    Upgraded to Compose Desktop 0.4.0.

--- build.gradle.kts
+++ build.gradle.kts
@@ -1,11 +1,14 @@
+import com.github.benmanes.gradle.versions.updates.DependencyUpdatesTask
+import com.github.benmanes.gradle.versions.updates.gradle.GradleReleaseChannel.CURRENT
 import org.gradle.api.tasks.wrapper.Wrapper.DistributionType.ALL
 import org.jetbrains.compose.compose
 import org.jetbrains.compose.desktop.application.dsl.TargetFormat
 import org.jetbrains.kotlin.gradle.tasks.KotlinCompile
 
 plugins {
-	kotlin("jvm") version "1.4.31"
-	id("org.jetbrains.compose") version "0.3.2"
+	kotlin("jvm") version "1.5.10"
+	id("org.jetbrains.compose") version "0.4.0"
+	id("com.github.ben-manes.versions") version "0.39.0"
 }
 
 group = "org.sdkotlin"
@@ -14,7 +17,7 @@ version = "1.0.0"
 dependencies {
 	implementation(compose.desktop.currentOs)
 
-	implementation(platform("org.junit:junit-bom:5.7.1"))
+	implementation(platform("org.junit:junit-bom:5.7.2"))
 
 	testImplementation("org.junit.jupiter:junit-jupiter-api")
 	testRuntimeOnly("org.junit.jupiter:junit-jupiter-engine")
@@ -29,8 +32,15 @@ tasks {
 		kotlinOptions.jvmTarget = "11"
 	}
 
+	withType<DependencyUpdatesTask> {
+		rejectVersionIf {
+			isNonStable(candidate.version)
+		}
+		gradleReleaseChannel = CURRENT.id
+	}
+
 	named<Wrapper>("wrapper") {
-		gradleVersion = "6.8.3"
+		gradleVersion = "7.1"
 		distributionType = ALL
 	}
 }
@@ -43,4 +53,16 @@ compose.desktop {
 			packageName = "compose-desktop-demo"
 		}
 	}
+}
+
+fun isNonStable(version: String): Boolean {
+	val stableKeyword = listOf("RELEASE", "FINAL", "GA").any {
+		version.toUpperCase()
+			.contains(it)
+	}
+	val unstableKeyword =
+		listOf("""M\d+""").any { version.toUpperCase().contains(it.toRegex()) }
+	val regex = "^[0-9,.v-]+(-r)?$".toRegex()
+	val isStable = (stableKeyword && !unstableKeyword) || regex.matches(version)
+	return isStable.not()
 }
