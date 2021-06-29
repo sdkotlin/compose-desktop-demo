5df46c248cff9f2a90fa77b3ba9ba08e9daa2224
Ian Brandt <ian.r.brandt@saic.com>
2021-06-29 08:57:01 -0700

    Switched to new Window API and added tooltip.

--- src/main/kotlin/org/sdkotlin/compose/Main.kt
+++ src/main/kotlin/org/sdkotlin/compose/Main.kt
@@ -1,23 +1,34 @@
 package org.sdkotlin.compose
 
-import androidx.compose.desktop.Window
-import androidx.compose.foundation.layout.Arrangement
-import androidx.compose.foundation.layout.Column
-import androidx.compose.foundation.layout.fillMaxHeight
-import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.BoxWithTooltip
+import androidx.compose.foundation.layout.*
+import androidx.compose.foundation.shape.RoundedCornerShape
 import androidx.compose.material.Button
 import androidx.compose.material.MaterialTheme
+import androidx.compose.material.Surface
 import androidx.compose.material.Text
 import androidx.compose.runtime.getValue
 import androidx.compose.runtime.mutableStateOf
 import androidx.compose.runtime.remember
 import androidx.compose.runtime.setValue
 import androidx.compose.ui.Alignment
+import androidx.compose.ui.ExperimentalComposeUiApi
 import androidx.compose.ui.Modifier
-import androidx.compose.ui.unit.IntSize
+import androidx.compose.ui.draw.shadow
+import androidx.compose.ui.unit.DpOffset
+import androidx.compose.ui.unit.dp
+import androidx.compose.ui.window.Window
+import androidx.compose.ui.window.WindowSize
+import androidx.compose.ui.window.application
+import androidx.compose.ui.window.rememberWindowState
 
-fun main() {
-	Window(title = "Demo", size = IntSize(width = 1024, height = 768)) {
+@ExperimentalComposeUiApi
+fun main() = application {
+
+	val windowState = rememberWindowState(size = WindowSize(width = 1024.dp, height = 768.dp))
+
+	Window(title = "Demo", state = windowState) {
+
 		var text by remember { mutableStateOf("Hello, World!") }
 
 		MaterialTheme {
@@ -26,10 +37,26 @@ fun main() {
 				horizontalAlignment = Alignment.CenterHorizontally,
 				verticalArrangement = Arrangement.Center
 			) {
-				Button(onClick = {
-					text = "Hello, Desktop!"
-				}) {
-					Text(text)
+				BoxWithTooltip(
+					tooltip = {
+						Surface(
+							modifier = Modifier.shadow(4.dp),
+							shape = RoundedCornerShape(4.dp)
+						) {
+							Text(
+								text = "Click me!",
+								modifier = Modifier.padding(10.dp)
+							)
+						}
+					},
+					delay = 500, // in milliseconds
+					offset = DpOffset((-8).dp, 4.dp) // tooltip offset
+				) {
+					Button(onClick = {
+						text = "Hello, Desktop!"
+					}) {
+						Text(text)
+					}
 				}
 			}
 		}
